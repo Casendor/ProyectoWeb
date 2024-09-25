134146582249c5d3fb69d62cc152283968a02209
Carlos Alejandre <carlos.alejandre@telefonica.com>
2024-09-25 14:40:20 +0200

    prueba

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const headers = document.querySelectorAll('.accordion-header');
+const headers = document.querySelectorAll(
+    ".accordion-header, .accordion-subheader"
+);
 
-headers.forEach(header => {
-    header.addEventListener('click', () => {
+headers.forEach((header) => {
+    header.addEventListener("click", () => {
         const content = header.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
